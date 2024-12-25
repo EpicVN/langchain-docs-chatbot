0f3ce5a776e4584bf5d793642934cd1d19a57ec8
EpicVN <ledangkhoahi3@gmail.com>
2024-12-25 14:09:08 +0700

    6th commit: Vector embeddings + vector store (AstraDB)

---
+++ lib/astradb.ts
@@ -0,0 +1,42 @@
+import { AstraDBVectorStore, AstraLibArgs } from "@langchain/community/vectorstores/astradb";
+import { OpenAIEmbeddings } from "@langchain/openai";
+import { DataAPIClient, Db, Collection, VectorizeDoc } from "@datastax/astra-db-ts";
+
+const endpoint = process.env.ASTRA_DB_API_ENDPOINT || "";
+const token = process.env.ASTRA_DB_APPLICATION_TOKEN || "";
+const collection = process.env.ASTRA_DB_COLLECTION || "embeddings";
+
+if (!token || !endpoint) {
+  throw new Error(
+    "Please set ASTRA_DB_ENDPOINT, ASTRA_DB_APPLICATION_TOKEN and ASTRA_DB_COLLECTION",
+  );
+}
+
+export async function getVectorStore() {
+  return AstraDBVectorStore.fromExistingIndex(
+    new OpenAIEmbeddings({ modelName: "text-embedding-3-small" }),
+    {
+      token,
+      endpoint,
+      collection,
+      collectionOptions: {
+        vector: {
+          dimension: 1536,
+          metric: "cosine",
+        },
+      },
+    },
+  );
+}
+
+export function getEmbeddingsCollection() {
+  // Create an instance of the `DataAPIClient` class with your token.
+  const client = new DataAPIClient(token);
+
+  // Get the database specified by your endpoint.
+  const database = client.db(endpoint);
+
+  console.log(`Connected to database ${database.id}`);
+
+  return database.collection(collection);
+}
--- scripts/generate.ts
+++ scripts/generate.ts
@@ -1,10 +1,18 @@
+import dotenv from "dotenv";
+dotenv.config({ path: ".env.local" });
+
 import { DirectoryLoader } from "langchain/document_loaders/fs/directory";
 import { TextLoader } from "langchain/document_loaders/fs/text";
 
 import { DocumentInterface } from "@langchain/core/documents";
 import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+import { getEmbeddingsCollection, getVectorStore } from "../lib/astradb";
 
 async function generateEmbeddings() {
+  const vectorStores = await getVectorStore();
+
+  (await getEmbeddingsCollection()).deleteMany({});
+
   const loader = new DirectoryLoader(
     "contents/docs/",
     {
@@ -22,22 +30,22 @@ async function generateEmbeddings() {
           .split("/contents/docs")[1]
           .split("/index")[0] || "/";
 
-      const pageContentTrimmed = doc.pageContent
-        // .replace(/^import.*$/gm, "")
-        // // Remove export statements
-        // .replace(/^export.*$/gm, "")
-        // // Remove React component props
-        // .replace(/ className=(["']).*?\1| className{.*?}/g, "")
-        // // Remove other common React props
-        // .replace(/ props={.*?}| {...props}/g, "")
-        // // Remove JSX component tags
-        // .replace(/<([A-Z][A-Za-z]+).*?>.*?<\/\1>/g, "")
-        // // Remove self-closing JSX tags
-        // .replace(/<([A-Z][A-Za-z]+).*?\/>/g, "")
-        // // Remove empty lines
-        // .replace(/^\s*[\r\n]/gm, "")
-        // // Remove multiple consecutive empty lines
-        // .replace(/\n\s*\n/g, "\n");
+      const pageContentTrimmed = doc.pageContent;
+      // .replace(/^import.*$/gm, "")
+      // // Remove export statements
+      // .replace(/^export.*$/gm, "")
+      // // Remove React component props
+      // .replace(/ className=(["']).*?\1| className{.*?}/g, "")
+      // // Remove other common React props
+      // .replace(/ props={.*?}| {...props}/g, "")
+      // // Remove JSX component tags
+      // .replace(/<([A-Z][A-Za-z]+).*?>.*?<\/\1>/g, "")
+      // // Remove self-closing JSX tags
+      // .replace(/<([A-Z][A-Za-z]+).*?\/>/g, "")
+      // // Remove empty lines
+      // .replace(/^\s*[\r\n]/gm, "")
+      // // Remove multiple consecutive empty lines
+      // .replace(/\n\s*\n/g, "\n");
 
       return {
         pageContent: pageContentTrimmed,
@@ -62,7 +70,7 @@ async function generateEmbeddings() {
 
   const splitDocs = await splitter.splitDocuments(docs);
 
-  console.log(splitDocs);
+  await vectorStores.addDocuments(splitDocs);
 }
 
 generateEmbeddings();
